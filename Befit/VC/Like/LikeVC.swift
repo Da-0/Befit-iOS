6f32294cff751bb646cee887ec8cbbffcc14d2f6
GGOMMI <lcs3303@gmail.com>
2018-12-25 21:32:02 +0900

    Add(Like): add LikeVC and storyboard reference (Main to Like)

---
+++ Befit/VC/Like/LikeVC.swift
@@ -0,0 +1,20 @@
+//
+//  LikeVC.swift
+//  Befit
+//
+//  Created by 이충신 on 25/12/2018.
+//  Copyright © 2018 GGOMMI. All rights reserved.
+//
+
+import UIKit
+
+class LikeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
