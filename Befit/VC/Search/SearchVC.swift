3a849cbb4772745559c45f2023c25d7475f24c8d
GGOMMI <lcs3303@gmail.com>
2018-12-25 21:23:55 +0900

    Add(Search): add SearchVC and storyboard reference (Main to Search)

---
+++ Befit/VC/Search/SearchVC.swift
@@ -0,0 +1,19 @@
+//
+//  SearchVC.swift
+//  Befit
+//
+//  Created by 이충신 on 25/12/2018.
+//  Copyright © 2018 GGOMMI. All rights reserved.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
