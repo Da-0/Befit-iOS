2373e973da08c45211c50c57e11abf7575eaf5ae
GGOMMI <lcs3303@gmail.com>
2018-12-25 17:21:24 +0900

    Initial Commit

---
+++ Befit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Befit
+//
+//  Created by 이충신 on 25/12/2018.
+//  Copyright © 2018 GGOMMI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
