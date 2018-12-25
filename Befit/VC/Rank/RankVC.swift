c79594da50222cf78cb253c011151b342b332100
GGOMMI <lcs3303@gmail.com>
2018-12-25 21:28:19 +0900

    Add(Rank): add RankVC and storyboard reference (Main to Rank)

---
+++ Befit/VC/Rank/RankVC.swift
@@ -0,0 +1,20 @@
+//
+//  RankVC.swift
+//  Befit
+//
+//  Created by 이충신 on 25/12/2018.
+//  Copyright © 2018 GGOMMI. All rights reserved.
+//
+
+import UIKit
+
+class RankVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
