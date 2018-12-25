917f32b7f05a1b958cfe8e6ee963237c525f0304
GGOMMI <lcs3303@gmail.com>
2018-12-25 21:50:43 +0900

    Add(Main): add CategoryVC and connect with sideMenuController to show side menu bar

---
+++ Befit/VC/Main/CategoryVC.swift
@@ -0,0 +1,20 @@
+//
+//  CategoryVC.swift
+//  Befit
+//
+//  Created by 이충신 on 25/12/2018.
+//  Copyright © 2018 GGOMMI. All rights reserved.
+//
+
+// Category 사이드 메뉴를 생성하기 위함(테이블 뷰 필요)
+import UIKit
+
+class CategoryVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
