7430d33ca20796e03ad486a750f8d045ad7951ed
GGOMMI <lcs3303@gmail.com>
2018-12-25 21:36:10 +0900

    Add(MyPage): add MyPageVC and storyboard reference (Main to MyPage)

---
+++ Befit/VC/MyPage/MyPageVC.swift
@@ -0,0 +1,19 @@
+//
+//  MyPageVC.swift
+//  Befit
+//
+//  Created by 이충신 on 25/12/2018.
+//  Copyright © 2018 GGOMMI. All rights reserved.
+//
+
+import UIKit
+
+class MyPageVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
