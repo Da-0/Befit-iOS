955de9d5ff5312886c63c8532a5204d6448a5b18
GGOMMI <lcs3303@gmail.com>
2018-12-28 02:03:39 +0900

    Add(Like): Import pager tab and make demo

---
+++ Befit/VC/Like/LikeProductCVC.swift
@@ -0,0 +1,89 @@
+//
+//  LikeProductCVC.swift
+//  Befit
+//
+//  Created by 이충신 on 27/12/2018.
+//  Copyright © 2018 GGOMMI. All rights reserved.
+//
+
+import UIKit
+import XLPagerTabStrip
+
+private let reuseIdentifier = "Cell"
+
+class LikeProductCVC: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Register cell classes
+        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    // MARK: UICollectionViewDataSource
+
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return 5
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+    
+        // Configure the cell
+    
+        return cell
+    }
+
+    // MARK: UICollectionViewDelegate
+
+    /*
+    // Uncomment this method to specify if the specified item should be highlighted during tracking
+    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment this method to specify if the specified item should be selected
+    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
+    
+    }
+    */
+
+}
+
+
+extension LikeProductCVC: IndicatorInfoProvider{
+    
+    func indicatorInfo(for pagerTabStripController: PagerTabStripViewController) -> IndicatorInfo {
+        return IndicatorInfo(title: "상품")
+    }
+}
--- Befit/VC/Like/LikeVC.swift
+++ Befit/VC/Like/LikeVC.swift
@@ -14,36 +14,33 @@ class LikeVC: ButtonBarPagerTabStripViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         configureButtonBar()
-        // Do any additional setup after loading the view.
     }
-    
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        moveToViewController(at: 1)
+        moveToViewController(at: 0)
         
     }
-    @IBAction func categoryAction(_ sender: Any) {
-         self.sideMenuController?.revealMenu();
-    }
+
     
     override func viewControllers(for pagerTabStripController: PagerTabStripViewController) -> [UIViewController]{
         
-        let child1 = UIStoryboard(name: "Like", bundle: nil).instantiateViewController(withIdentifier: "LikeProductTVC")
-        
-        let child2 = UIStoryboard(name: "Like", bundle: nil).instantiateViewController(withIdentifier: "LikeBrandTVC")
+        let child1 = UIStoryboard(name: "Like", bundle: nil).instantiateViewController(withIdentifier: "LikeProductCVC") as! LikeProductCVC
+        let child2 = UIStoryboard(name: "Like", bundle: nil).instantiateViewController(withIdentifier: "LikeBrandTVC") as! LikeBrandTVC
         
         return [child1, child2]
     }
     
     func configureButtonBar() {
+        
         // Sets the background colour of the pager strip and the pager strip item
         settings.style.buttonBarBackgroundColor = .white
         settings.style.buttonBarItemBackgroundColor = .white
         
         // Sets the pager strip item font and font color
-        settings.style.buttonBarItemFont = UIFont(name: "System", size: 16.0)!
-        settings.style.buttonBarItemTitleColor = .gray
+        settings.style.buttonBarItemFont = UIFont(name: "Helvetica", size: 14.0)!
+        settings.style.buttonBarItemTitleColor = .black
+        
         
         // Sets the pager strip item offsets
         settings.style.buttonBarMinimumLineSpacing = 0
@@ -52,13 +49,15 @@ class LikeVC: ButtonBarPagerTabStripViewController {
         settings.style.buttonBarRightContentInset = 0
         
         // Sets the height and colour of the slider bar of the selected pager tab
-        settings.style.selectedBarHeight = 3.0
-        settings.style.selectedBarBackgroundColor = .black
+        settings.style.selectedBarHeight = 1.0
+        settings.style.selectedBarBackgroundColor = .black // 선택되었을경우
         
-        settings.style.buttonBarBackgroundColor = .white
         
-        containerView.frame.origin.y = 104
-        buttonBarView.frame.contains(CGRect(x: 0, y: 60, width: 375, height: 667))
+        settings.style.selectedBarVerticalAlignment = .bottom //****
+        
+        
+        //containerView.frame.origin.y = 104//****
+        //buttonBarView.frame.contains(CGRect(x: 0, y: 300, width: 375, height: 812))//****
         
         // Changing item text color on swipe
         changeCurrentIndexProgressive = { (oldCell: ButtonBarViewCell?, newCell: ButtonBarViewCell?, progressPercentage: CGFloat, changeCurrentIndex: Bool, animated: Bool) -> Void in
@@ -68,4 +67,11 @@ class LikeVC: ButtonBarPagerTabStripViewController {
         }
     }
 }
+
+
+
+
+
+
+
 
