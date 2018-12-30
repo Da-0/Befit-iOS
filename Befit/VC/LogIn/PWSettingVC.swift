29bd844bbb3833b187b9eee5ccf1e1dbf832ea69
Da-0 <dayoung814@naver.com>
2018-12-30 13:33:27 +0900

    Add(LogIn): add PWSettingVC.switf and update LogIn.storyboard(PWSettingVC)

---
+++ Befit/VC/LogIn/PWSettingVC.swift
@@ -0,0 +1,20 @@
+//
+//  PWSettingVC.swift
+//  Befit
+//
+//  Created by 박다영 on 30/12/2018.
+//  Copyright © 2018 GGOMMI. All rights reserved.
+//
+
+import UIKit
+
+class PWSettingVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
