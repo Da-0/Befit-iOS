9a297df6bd4c33ef523f8ee0d0e6f7e59a6579e2
GGOMMI <lcs3303@gmail.com>
2018-12-31 11:33:14 +0900

    Add(Rank): add rank view

--- Befit/VC/Rank/RankVC.swift
+++ Befit/VC/Rank/RankVC.swift
@@ -9,12 +9,35 @@
 import UIKit
 
 class RankVC: UIViewController {
-
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableView.delegate = self;
+        tableView.dataSource = self;
         // Do any additional setup after loading the view.
     }
     
 
+}
+
+
+extension RankVC: UITableViewDelegate, UITableViewDataSource {
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 9
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "RankTVCell", for: indexPath) as! RankTVCell
+        cell.rankLB.text = "\(indexPath.row + 1)"
+        
+        return cell
+        
+    }
+    
+    
+    
 }
