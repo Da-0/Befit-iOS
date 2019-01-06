5e79835ec92e1feb5de0009095cc81f72757ce20
Da-0 <dayoung814@naver.com>
2019-01-06 17:31:56 +0900

    Update(LogIn): Description Add(Rank): Add BrandVC

---
+++ Befit/VC/Rank/BrandVC.swift
@@ -0,0 +1,96 @@
+//
+//  BrandVC.swift
+//  Befit
+//
+//  Created by 박다영 on 06/01/2019.
+//  Copyright © 2019 GGOMMI. All rights reserved.
+//
+
+import UIKit
+
+class BrandVC: UIViewController {
+
+    @IBOutlet weak var BrandLogoImg: UIImageView!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var categoryName: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        BrandLogoImg.setRounded()
+        
+        collectionView.delegate = self;
+        collectionView.dataSource = self;
+    }
+   
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.navigationBar.isHidden = true
+        self.tabBarController?.tabBar.isHidden = true
+//        navigationBar.topItem?.title = categoryName
+        }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.navigationController?.navigationBar.isHidden = false
+        self.tabBarController?.tabBar.isHidden = false
+        }
+    
+    @IBAction func backAction(_ sender: Any) {
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+}
+
+extension UIImageView {
+    
+    func setRounded() {
+        self.layer.cornerRadius = (self.frame.width / 2) //instead of let radius = CGRectGetWidth(self.frame) / 2
+        self.layer.masksToBounds = true
+    }
+}
+
+
+
+extension BrandVC: UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CategoryDetailCVCell", for: indexPath) as! CategoryDetailCVCell
+        
+        cell.productImg.image = #imageLiteral(resourceName: "testImage")
+        cell.brandName.text = "헬로월드"
+        cell.productName.text = "옷옷옷"
+        cell.price.text = "150,000"
+        
+        return cell
+    }
+    
+}
+
+
+
+
+
+extension BrandVC: UICollectionViewDelegateFlowLayout{
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+         //iphone사이즈에 따라 동적으로 대응이 가능해진다.
+        // let width: CGFloat = (self.collectionView.frame.width ) / 2 - 20
+        // let height: CGFloat =  (self.collectionView.frame.height ) / 2 - 20
+        return CGSize(width: 167, height: 235)
+   }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 9
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 15, bottom: 15, right: 15)
+    }
+    
+}
+
