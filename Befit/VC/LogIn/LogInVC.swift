64684b0351f7e327c20fe076eded918cab49262b
GGOMMI <lcs3303@gmail.com>
2019-01-06 14:00:34 +0900

    Update(LogIn): update login network, get token value successful

--- Befit/VC/LogIn/LogInVC.swift
+++ Befit/VC/LogIn/LogInVC.swift
@@ -8,15 +8,50 @@
 
 import UIKit
 
-class LogInVC: UIViewController {
+class LogInVC: UIViewController, APIManager {
 
+    @IBOutlet weak var emailTF: UITextField!
+    @IBOutlet weak var pwTF: UITextField!
+    
+    let userDefault = UserDefaults.standard
     @IBOutlet weak var switchBtn: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         switchBtn.transform = CGAffineTransform(scaleX: 0.63, y: 0.63)
     }
     
+    @IBAction func loginBtn(_ sender: Any){
+        
+        if (emailTF.text?.isEmpty)! || (pwTF.text?.isEmpty)! {
+            simpleAlert(title: "로그인 실패", message: "모든 항목을 입력해 주세요")
+            return
+        }
+        
+        network()
+    }
+    
+    
+    func network(){
+        
+        LoginService.shared.login(email: emailTF.text!, password: pwTF.text!, completion: {[weak self] (res) in
+            guard let `self` = self else {return}
+            
+            if res.token != nil {
+                
+                self.userDefault.set(res.token!, forKey: "token")
+                
+                print(res.token!)
+                
+                let mainVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "sideStart")
+                
+                self.present(mainVC, animated: true, completion: nil)
+            }
+            
+        })
+        
+    }
     
 
 
