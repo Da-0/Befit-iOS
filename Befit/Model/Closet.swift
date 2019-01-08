6dd539e2514202e13662f5b5592b233f80db0723
GGOMMI <lcs3303@gmail.com>
2019-01-08 16:53:16 +0900

    Add(MyPage): add Closet Model

--- Befit/Model/Closet.swift
+++ Befit/Model/Closet.swift
@@ -7,3 +7,33 @@
 //
 
 import Foundation
+import ObjectMapper
+
+struct Closet : Mappable {
+    
+    var closet_idx : Int?
+    var name_korean : String?
+    var name_english : String?
+    var name : String?
+    var image_url : String?
+    var product_category_index : Int?
+    var product_size : String?
+    var measure : Measure?
+    
+    init?(map: Map) {
+        
+    }
+    
+    mutating func mapping(map: Map) {
+        
+        closet_idx <- map["closet_idx"]
+        name_korean <- map["name_korean"]
+        name_english <- map["name_english"]
+        name <- map["name"]
+        image_url <- map["image_url"]
+        product_category_index <- map["product_category_index"]
+        product_size <- map["product_size"]
+        measure <- map["measure"]
+    }
+    
+}
