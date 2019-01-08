10802ae11bdd5f265e4b078df22f8f045d022652
GGOMMI <lcs3303@gmail.com>
2019-01-08 16:54:44 +0900

    Add(MyPage): add ProductSelectService

--- Befit/Network/Service/MyPage/SizeInfo/BrandSelectService.swift
+++ Befit/Network/Service/MyPage/SizeInfo/BrandSelectService.swift
@@ -6,4 +6,34 @@
 //  Copyright © 2019 GGOMMI. All rights reserved.
 //
 
-import Foundation
+import Alamofire
+
+struct BrandSelectService: APIManager, Requestable{
+    
+    typealias NetworkData = ResponseArray<Brand>
+    static let shared = BrandSelectService()
+    
+    let URL = url("/brands")
+    
+    let headers: HTTPHeaders = [
+        "Authorization" : UserDefaults.standard.string(forKey: "token")!
+    ]
+    
+    //이니셜로 검색하는 경우 브랜드 리스트를 출력 해주는 api
+    func showBrandList(completion: @escaping ([Brand]?) -> Void) {
+       let brandInitial = UserDefaults.standard.string(forKey: "brand_initial")!
+       let queryURL = URL + "?initial=" + brandInitial
+        print(queryURL)
+        gettable(queryURL, body: nil, header: headers) { res in
+            switch res {
+            case .success(let value):
+                completion(value.data)
+            case .error(let error):
+                print(error)
+            }
+        }
+        
+    }
+    
+    
+}
