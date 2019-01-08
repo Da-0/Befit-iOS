8978301f251f079f57b6a2d8703dcba82fe60feb
GGOMMI <lcs3303@gmail.com>
2019-01-08 16:55:21 +0900

    Add(MyPage): add ProductSelectService

--- Befit/Network/Service/MyPage/SizeInfo/ProductSelectService.swift
+++ Befit/Network/Service/MyPage/SizeInfo/ProductSelectService.swift
@@ -6,4 +6,32 @@
 //  Copyright © 2019 GGOMMI. All rights reserved.
 //
 
-import Foundation
+import Alamofire
+
+struct ProductSelectService: APIManager, Requestable{
+    
+    typealias NetworkData = ResponseArray<Closet>
+    static let shared = ProductSelectService()
+    
+    let URL = url("/closet/category/" + "\(UserDefaults.standard.integer(forKey: "category_idx"))")
+    
+    let headers: HTTPHeaders = [
+        "Authorization" : UserDefaults.standard.string(forKey: "token")!
+    ]
+    
+    //해당 카테고리의 등록된 옷 리스트 출력
+    func showClosetList(completion: @escaping ([Closet]?) -> Void) {
+        
+        gettable(URL, body: nil, header: headers) { res in
+            switch res {
+            case .success(let value):
+                completion(value.data)
+            case .error(let error):
+                print(error)
+            }
+        }
+        
+    }
+    
+    
+}
