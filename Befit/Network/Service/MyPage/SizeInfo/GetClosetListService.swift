deb8dd58273a73f33eced729d5b48a3d82eea91c
GGOMMI <lcs3303@gmail.com>
2019-01-08 16:54:02 +0900

    Add(MyPage): add closetListService

--- Befit/Network/Service/MyPage/SizeInfo/GetClosetListService.swift
+++ Befit/Network/Service/MyPage/SizeInfo/GetClosetListService.swift
@@ -6,4 +6,33 @@
 //  Copyright © 2019 GGOMMI. All rights reserved.
 //
 
-import Foundation
+
+import Alamofire
+
+struct GetClosetListService: APIManager, Requestable{
+    
+    typealias NetworkData = ResponseArray<Closet>
+    static let shared = GetClosetListService()
+    
+    let URL = url("/closet/category/" + "\(UserDefaults.standard.integer(forKey: "category_idx"))")
+    
+    let headers: HTTPHeaders = [
+        "Authorization" : UserDefaults.standard.string(forKey: "token")!
+    ]
+    
+    //해당 카테고리의 등록된 옷 리스트 출력
+func showClosetList(completion: @escaping ([Closet]?) -> Void) {
+        
+        gettable(URL, body: nil, header: headers) { res in
+            switch res {
+            case .success(let value):
+                completion(value.data)
+            case .error(let error):
+                print(error)
+            }
+        }
+        
+    }
+    
+    
+}
