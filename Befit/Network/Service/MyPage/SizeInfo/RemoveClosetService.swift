df21bdab2a2603e4d565bbd1b5db1df5da0a74f6
GGOMMI <lcs3303@gmail.com>
2019-02-04 15:27:28 +0900

    Add(MyPage): implement sign out function

--- Befit/Network/Service/MyPage/SizeInfo/RemoveClosetService.swift
+++ Befit/Network/Service/MyPage/SizeInfo/RemoveClosetService.swift
@@ -6,4 +6,43 @@
 //  Copyright © 2019 GGOMMI. All rights reserved.
 //
 
-import Foundation
+import Alamofire
+
+struct RemoveClosetService: APIManager, Requestable{
+    
+    let userDefault = UserDefaults.standard
+    typealias NetworkData = ResponseObject<Token>
+    static let shared = RemoveClosetService()
+    
+    let baseURL = url("/closet/")
+    
+    //상품 좋아요 취소
+    func removeCloset(closetIdx: [Int], completion: @escaping (NetworkData) -> Void) {
+        
+        var realString = ""
+        for (idx, val) in closetIdx.enumerated(){
+            if idx == 0 { realString += "\(val)"}
+            else{ realString += ",\(val)"}
+        }
+        
+        let url = baseURL + realString
+        guard let token = userDefault.string(forKey: "token") else {return}
+        
+        let headers: HTTPHeaders = [
+            "Authorization" : token
+        ]
+        
+        delete(url, body: nil, header: headers) { res in
+            switch res {
+                case .success(let value):
+                    completion(value)
+                case .error(let error):
+                    print(error)
+            }
+        }
+        
+    }
+    
+    
+    
+}
