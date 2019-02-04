61e2f73494e7930c97b0403e9d113fa8e2922f32
GGOMMI <lcs3303@gmail.com>
2019-02-04 15:33:49 +0900

    Add(All): add shadow effect in CollectionView Cell

--- Befit/View/MyPage/SizeInfo2TabCVCell.swift
+++ Befit/View/MyPage/SizeInfo2TabCVCell.swift
@@ -9,5 +9,19 @@
 import UIKit
 
 class SizeInfo2TabCVCell: UICollectionViewCell {
+    @IBOutlet weak var tabImg: UIImageView!
     
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.dropShadow()
+    }
+    
+    func dropShadow() {
+        self.layer.masksToBounds = false
+        self.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.25).cgColor
+        self.layer.shadowOpacity = 0.5
+        self.layer.shadowOffset = CGSize(width: 0.0, height: 2.0)
+        self.layer.shadowRadius = 4.0
+        
+    }
 }
--- Befit/View/Search/SearchCVCell.swift
+++ Befit/View/Search/SearchCVCell.swift
@@ -12,4 +12,17 @@ class SearchCVCell: UICollectionViewCell {
     
     @IBOutlet weak var productImg: UIImageView!
     
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.dropShadow()
+    }
+    
+    func dropShadow() {
+        self.layer.masksToBounds = false
+        self.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.25).cgColor
+        self.layer.shadowOpacity = 0.5
+        self.layer.shadowOffset = CGSize(width: 0.0, height: 2.0)
+        self.layer.shadowRadius = 4.0
+        
+    }
 }
